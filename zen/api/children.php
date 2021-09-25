2c846dcc0f508bbf394f1a0bf601490ab89d931f
KKS <kirill.kuliuckin2016@yandex.ru>
2021-09-25 19:33:48 +0300

    API in hosting

--- zen/api/children.php
+++ zen/api/children.php
@@ -1,5 +1,5 @@
 <?php
 setcookie("childid",1);
-$children=file_get_contents("http://dobroapi/api/children/get.php?id=2");
+$children=file_get_contents("http://f0578562.xsph.ru/api/children/get.php?id=2");
 echo $children;
 ?>
--- zen/api/events.php
+++ zen/api/events.php
@@ -1,1 +1,4 @@
 <?php
+$events=file_get_contents("http://f0578562.xsph.ru/api/event/getList.php");
+echo $events;
+?>
